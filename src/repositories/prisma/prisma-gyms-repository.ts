1ba14ae4c5da9206aaebde4f70cb59075cc9efa0
damattag <guilherme.damatta@yahoo.com.br>
2023-06-13 20:39:12 -0300

    create gyms prisma repository

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,47 @@
+import { Gym, Prisma } from "@prisma/client";
+import { GymsRepository, findManyNerbyParams } from "../gyms-repository";
+import { prisma } from "@/lib/prisma";
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return gym;
+  }
+
+  async findManyNerby({ latitude, longitude }: findManyNerbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * FROM gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `;
+
+    return gyms;
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        name: {
+          contains: query,
+          mode: "insensitive",
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    });
+
+    return gyms;
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    });
+
+    return gym;
+  }
+}
