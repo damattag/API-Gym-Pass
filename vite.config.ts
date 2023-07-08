59511938e919f963a82985c137494cd16ec31ac8
Guilherme da Matta <guilherme.damatta@yahoo.com.br>
2023-07-08 10:47:33 -0300

    fixed VITE.CONFIG

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     environmentMatchGlobs: [['src/http/controllers/**/*.test.ts', 'prisma']],
-
+    include: ['src/**/*.test.ts'],
   },
 });
